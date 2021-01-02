bc5f89b6a274af2e30ed841ef35c7d20fad68277
Roberto B <roberto.butti@gmail.com>
2021-01-02 22:13:05 +0100

    update app.name config

--- config/app.php
+++ config/app.php
@@ -13,7 +13,9 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    //'name' => env('APP_NAME', 'Laravel'),
+    'name' => "Laravel GitHub Actions Yaml Generator",
+
 
     /*
     |--------------------------------------------------------------------------
