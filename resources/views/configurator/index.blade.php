a593504c428c4eaddc7e2df9439e8e832d7a18bd
Roberto B <roberto.butti@gmail.com>
2021-01-18 22:20:30 +0100

    fix index.php with og meta and indenting

--- resources/views/configurator/index.blade.php
+++ resources/views/configurator/index.blade.php
@@ -1,40 +1,38 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>{{ config("app.name") }} - GitHub Actions Yaml Generator</title>
-        <!-- Fonts -->
-      <!--
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-        <style>
-            body {
-                font-family: 'Nunito';
-            }
-        </style>
-        -->
-        <livewire:styles />
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.0/dist/alpine.min.js" defer></script>
-    </head>
-    <body class="antialiased">
-      <div>
-        <header class="bg-white shadow">
-          <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-            <h1 class="text-3xl font-bold leading-tight text-gray-900">
-              Ghygen - Github Actions Yaml Generator (for Laravel)
-            </h1>
-          </div>
-        </header>
-        <main>
-          <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-          <livewire:configurator-form  />
-          </div>
-        </main>
-        <x-footer></x-footer>
-      </div>
-      <livewire:scripts />
-    </body>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>{{ config("app.name") }} - GitHub Actions Yaml Generator</title>
+  <livewire:styles/>
+  <meta charset="UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.0/dist/alpine.min.js" defer></script>
+  <meta property="og:url" content="{{ config('app.url') }}"/>
+  <meta property="og:type" content="website"/>
+  <meta property="og:title" content="{{ config("app.name") }} - GitHub Actions Yaml Generator"/>
+  <meta property="og:description" content="Create your GitHub Actions workflow for Laravel applications."/>
+  <meta property="og:image"
+        content="https://raw.githubusercontent.com/Hi-Folks/gh-actions-yaml-generator/main/ghygen-github-actions-yaml-generator-laravel.png"/>
+
+</head>
+<body class="antialiased">
+<div>
+  <header class="bg-white shadow">
+    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+      <h1 class="text-3xl font-bold leading-tight text-gray-900">
+        Ghygen - Github Actions Yaml Generator (for Laravel)
+      </h1>
+    </div>
+  </header>
+  <main>
+    <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+      <livewire:configurator-form/>
+    </div>
+  </main>
+  <x-footer></x-footer>
+</div>
+<livewire:scripts/>
+</body>
 </html>
