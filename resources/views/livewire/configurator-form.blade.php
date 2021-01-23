6e0f24859e387aa13a3751d6063d437f04b14033
Roberto B <roberto.butti@gmail.com>
2021-01-23 18:22:38 +0100

    change input colors, from indigo to blue

--- resources/views/livewire/configurator-form.blade.php
+++ resources/views/livewire/configurator-form.blade.php
@@ -301,13 +301,13 @@
 
           <div class="flex flex-row w-full text-right">
             <div class="flex-grow px-4 py-3 bg-gray-50 text-right sm:px-6">
-              <button  type="button" class="copy-btn inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              <button  type="button" class="copy-btn inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
                        data-clipboard-text="{{ $result }}">
                 Copy
               </button>
             </div>
             <div class="flex-grow-0 px-4 py-3 bg-gray-50 text-right sm:px-6">
-              <!--button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+              <!--button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
                 Generate Yaml File
               </button-->
 
@@ -316,7 +316,7 @@
               hljs.lineNumbersBlock(yc);
               hljs.highlightBlock(yc);
               })"
-              class="uppercase inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Generate Yaml File</button>
+              class="uppercase inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">Generate Yaml File</button>
             </div>
           </div>
         </div>
