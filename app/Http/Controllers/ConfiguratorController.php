eee971089c6875b08f652afe5b31d46b4d84a455
Roberto B <roberto.butti@gmail.com>
2021-02-16 08:23:43 +0100

    Auto Fix PSR12 Style

--- app/Http/Controllers/ConfiguratorController.php
+++ app/Http/Controllers/ConfiguratorController.php
@@ -10,7 +10,9 @@ class ConfiguratorController extends Controller
     {
         $data = [];
         $data["title"] = "Ghygen is a GitHub Actions configurator for your Laravel Application.";
-        $data["description"] = "Setup Database Service, use multiple PHP version, use multiple Laravel versions, build frontend, cache packages, execute Browser, Functional, and Unit tests…";
+        $data["description"] = "Setup Database Service, use multiple PHP version,
+            use multiple Laravel versions, build frontend, cache packages,
+            execute Browser, Functional, and Unit tests…";
         return view('configurator.index', $data);
     }
 }
