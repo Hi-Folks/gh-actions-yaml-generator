3d7dc6cc648fab2790021b54fdb70572868e7048
Roberto B <roberto.butti@gmail.com>
2021-05-13 07:14:56 +0200

    fix wild card
    For #75  issue , on branch names we need to escape names starts with * , [ and !
    https://docs.github.com/en/actions/reference/workflow-syntax-for-github-actions#filter-pattern-cheat-sheet

--- resources/views/yaml/on.blade.php
+++ resources/views/yaml/on.blade.php
@@ -6,14 +6,22 @@ on:
   push:
     branches:
 @foreach ( $on_push_branches as $branch)
+@if (Str::startsWith($branch, ['*', '[', '!']))
+      - '{{ $branch }}'
+@else
       - {{ $branch }}
+@endif
 @endforeach
 @endif
 @if ( $on_pullrequest )
   pull_request:
     branches:
 @foreach ( $on_pullrequest_branches as $branch)
+@if (Str::startsWith($branch, ['*', '[', '!']))
+      - '{{ $branch }}'
+@else
       - {{ $branch }}
+@endif
 @endforeach
 @endif
 @endif
