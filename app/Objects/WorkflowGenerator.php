a7a5f366e1c31a9fa7e527603d102475ca031138
Roberto B <roberto.butti@gmail.com>
2022-04-17 11:01:55 +0200

    added php8.1 for autodetecting php version for matrix

--- app/Objects/WorkflowGenerator.php
+++ app/Objects/WorkflowGenerator.php
@@ -104,11 +104,11 @@ class WorkflowGenerator
     /**
      * @return string[]
      *
-     * @psalm-return list<'7.3'|'7.4'|'8.0'>
+     * @psalm-return list<'7.3'|'7.4'|'8.0'|'8.1'>
      */
     public function detectPhpVersion(string $phpversion): array
     {
-        $listPhpVersions = [ "7.3", "7.4", "8.0"];
+        $listPhpVersions = [ "7.3", "7.4", "8.0","8.1"];
         $stepPhp = [];
         foreach ($listPhpVersions as $php) {
             if (Semver::satisfies($php, $phpversion)) {
